926fc0a86b3dd467789b634ed0b8369ce2d08aea
Naura Natwa <eunoianats@gmail.com>
2026-07-07 10:41:22 +0700

    add feature

--- editdata.php
+++ editdata.php
@@ -1,16 +1,19 @@
 <?php
     require 'fungsi.php';
-    if (isset($_POST["submit"])) {
 
-        if (tambahdata($_POST, $_FILES["foto"]) > 0) {
+    $id = $_GET["id"];
+    $query = "SELECT * FROM mahasiswa WHERE id = $id";
+    $mahasiswa = tampildata($query)[0]; // data spesifik $id
+
+    if (isset($_POST["submit"])) {
+        if (editdata($_POST, $id) > 0) {
             echo "<script>
-                    alert('Data berhasil ditambahkan!');
+                    alert('Data berhasil diubah!');
                     document.location.href = 'data.php';
                 </script>";
-        } 
-        else {
+        } else {
             echo "<script>
-                    alert('Data gagal ditambahkan!');
+                    alert('Data gagal diubah!');
                     document.location.href = 'data.php';
                 </script>";
         }
@@ -22,12 +25,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Students</title>
+    <title>Edit Data</title>
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 
-    <h1 align="center">Students</h1>
+    <h1 align="center">Edit Data Mahasiswa</h1>
 
     <table border="1" align="center" cellpadding="10" cellspacing="0">
         <tr>
@@ -43,42 +46,42 @@
 
     <h2 align="center">Form Data Mahasiswa</h2>
 
-    <form action="" method="POST" align="center" enctype="multipart/form-data">
+    <form action="" method="POST" align="center">
         <table border="1" align="center" cellpadding="10" cellspacing="0">
             
             <tr>
                 <td>Nama</td>
-                <td><input type="text" name="nama"></td>
+                <td><input type="text" name="nama" value="<?= $mahasiswa["nama"]; ?>"></td>
             </tr>
 
             <tr>
                 <td>NIM</td>
-                <td><input type="number" name="nim"></td>
+                <td><input type="number" name="nim" value="<?= $mahasiswa["nim"]; ?>">
             </tr>
 
             <tr>
                 <td>Program Studi</td>
-                <td><input type="text" name="prodi"></td>
+                <td><input type="text" name="prodi" value="<?= $mahasiswa["prodi"]; ?>"></td>
             </tr>
 
             <tr>
                 <td>Email</td>
-                <td><input type="email" name="email"></td>
+                <td><input type="email" name="email" value="<?= $mahasiswa["email"]; ?>">
             </tr>
 
             <tr>
                 <td>No HP</td>
-                <td><input type="number" name="no_hp"></td>
+                <td><input type="number" name="no_hp" value="<?= $mahasiswa["no_hp"]; ?>"></td>
             </tr>
 
             <tr>
                 <td>Foto</td>
-                <td><input type="file" name="foto"></td>
+                <td><input type="file" name="foto" value="<?= $mahasiswa["foto"]; ?>"></td>
             </tr>
 
             <tr>
                 <td colspan="2" align="center">
-                    <input type="submit" name="submit" value="Send">
+                    <input type="submit" name="submit" value="Edit Data">
                 </td>
             </tr>
 
--- fungsi.php
+++ fungsi.php
@@ -14,25 +14,53 @@
 
     function deletedata($id) {
         global $koneksi;
+        
         mysqli_query($koneksi, "DELETE FROM mahasiswa WHERE id = $id");
         mysqli_affected_rows($koneksi, $query);
 
         return mysqli_affected_rows($koneksi);
     }
 
-    function tambahdata($data) {
+    function tambahdata($data, $files) {
         global $koneksi;
-        $id = $data["id"];
-        $nama =htmlspecialchars($data["nama"]);
-        $nama = $data["nama"];
-        $nim = $data["nim"];
-        $prodi = $data["prodi"];
-        $email = $data["email"];
-        $no_hp = $data["no_hp"];
-        $foto = $data["foto"];
 
+        $nama = htmlspecialchars($data["nama"]);
+        $nim = htmlspecialchars($data["nim"]);
+        $prodi = htmlspecialchars($data["prodi"]);
+        $email = htmlspecialchars($data["email"]);
+        $no_hp = htmlspecialchars($data["no_hp"]);
+
+        $namafoto = $_FILES["name"];
+        $tmpfoto = $_FILES["tmp_name"];
+        $path = "Assets/$namafoto";
+
+        if (move_uploaded_file($tmpfoto, $path)) {
         $query = "INSERT INTO mahasiswa (nama, nim, prodi, email, no_hp, foto) 
-        VALUES ('$nama', '$nim', '$prodi', '$email', '$no_hp', '$foto')";
+        VALUES ('$nama', '$nim', '$prodi', '$email', '$no_hp', '$namafoto')";
+        mysqli_query($koneksi, $query);
+        }
+
+        return mysqli_affected_rows($koneksi);
+    }
+
+    function editdata($data, $id) {
+        global $koneksi;
+
+        $nama = htmlspecialchars($data["nama"]);
+        $nim = htmlspecialchars($data["nim"]);
+        $prodi = htmlspecialchars($data["prodi"]);
+        $email = htmlspecialchars($data["email"]);
+        $no_hp = htmlspecialchars($data["no_hp"]);
+        $foto = htmlspecialchars($data["foto"]);
+
+        $query = "UPDATE mahasiswa SET 
+                    nama = '$nama',
+                    nim = '$nim',
+                    prodi = '$prodi',
+                    email = '$email',
+                    no_hp = '$no_hp',
+                    foto = '$foto'
+                WHERE id = $id";
 
         mysqli_query($koneksi, $query);
 
